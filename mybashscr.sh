ac73f69f806aa5a6d2a89c5c01cdde88c8469ced
Varuj2002 <varuzhan.hovsepyan.dwin@gmail.com>
2021-12-10 10:45:09 +0400

    09-08

--- mybashscr.sh
+++ mybashscr.sh
@@ -10,7 +10,12 @@ do
        for var2 in 0 1 2 3 4 5 6 7 8 9;
        do
             mkdir $var1/$var2
-       done    
+    	   
+    done    
+	f1=`expr $2 % 10`
+	f2=$((($2 - $f1) /10))
+	mv $1 $f2/$f1
 
 done   
 fi
+
